53ffa1e5cf278b3e7df2eb1405166c9a062641fc
Shameem <ansari.shameem80@gmail.com>
2021-04-22 01:06:38 +0530

    Fixed Bug - Overflow

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,17 @@
 const navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', (e) => {
-  if (window.pageYOffset > 50) {
+  if (window.pageYOffset > 0) {
     navbar.classList.add('show');
   } else {
     navbar.classList.remove('show');
   }
 });
 
-window.addEventListener('load', (e) => {
-  if (window.innerWidth < 992) {
-    navbar.classList.add('show');
-  } else {
-    navbar.classList.remove('show');
-  }
-});
+// window.addEventListener('load', (e) => {
+//   if (window.innerWidth < 992) {
+//     navbar.classList.add('show');
+//   } else {
+//     navbar.classList.remove('show');
+//   }
+// });

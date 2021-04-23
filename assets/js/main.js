532740630b5326ff811cbef45806bce7e4cbc66d
Shameem <ansari.shameem80@gmail.com>
2021-04-24 00:18:28 +0530

    Shameem

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,35 @@
 const navbar = document.querySelector('.navbar');
 
 window.addEventListener('scroll', (e) => {
-  if (window.pageYOffset > 0) {
+  if (window.innerWidth < 992) {
     navbar.classList.add('show');
   } else {
-    if (window.innerWidth > 992) {
+    if (window.pageYOffset > 0) {
+      navbar.classList.add('show');
+    } else {
       navbar.classList.remove('show');
     }
   }
 });
 
 window.addEventListener('load', (e) => {
-  if (window.innerWidth < 992) {
+  if (window.pageYOffset > 0) {
     navbar.classList.add('show');
   } else {
-    navbar.classList.remove('show');
+    if (window.innerWidth < 992) {
+      navbar.classList.add('show');
+    }
   }
 });
 
 window.addEventListener('resize', (e) => {
-  if (window.innerWidth < 992) {
+  if (window.pageYOffset > 0) {
     navbar.classList.add('show');
   } else {
-    navbar.classList.remove('show');
+    if (window.innerWidth < 992) {
+      navbar.classList.add('show');
+    } else {
+      navbar.classList.remove('show');
+    }
   }
 });
